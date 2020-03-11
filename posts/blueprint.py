b235093eefc8b478179e208a7b314611152daae2
vkhalaim <vasyl.khalaim@ukr.net>
2020-03-11 17:33:38 +0200

    Added template for post in blueprint.

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -9,5 +9,9 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 @posts.route('/')
 def index():
     posts = Post.query.all()
-
     return render_template('posts/index.html', posts=posts)
+
+@posts.route('/<slug>')
+def post_detail(slug):
+    post = Post.query.filter(Post.slug == slug).first()
+    return render_template('posts/post_detail.html', post=post)
